0bce09ff2b6784ee6978cb85b6e652d55e9a6cf3
said <said.elkharroubi@hotmail.com>
2020-08-17 14:16:23 +0200

    mvc & routing

---
+++ src/Controller/LearningController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LearningController extends AbstractController
+{
+    /**
+     * @Route("/about-me", name="aboutme")
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function aboutMe(SessionInterface $session): response
+    {
+        if ($session->get('name')) {
+            $name = $session->get('name');
+            return $this->render('aboutMe.html.twig', ['name' => $name]);
+        }
+            return $this->forward('App\Controller\LearningController::showMyName');
+    }
+    
+    /**
+     * @Route("/", name="showMyName")
+     * @param SessionInterface $session
+     * @return Response
+     */
+    public function showMyName(SessionInterface $session): response
+    {
+        if($session->get('name')){
+            $name = $session->get('name');
+        }else{
+            $name = 'unknown';
+        }
+
+        return $this->render('showMyName.html.twig', [
+        'name' => $name,]);
+    }
+
+    /**
+     * @Route("/changeMyName", name="changeMyName", methods={"POST"})
+     * @return RedirectResponse
+     * @param Request $request
+     * @param SessionInterface $session
+     */
+
+    public function changeMyName(Request $request, SessionInterface $session): RedirectResponse
+    {
+        $name = $request->request->get('name');
+        $session->set('name', $name);
+
+        return $this->redirectToRoute('showMyName');
+     }
+}
